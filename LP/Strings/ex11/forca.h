b73cafdee55d18a8f12239fb2ef36d2d1e8a629b
Pedro <99131505+pedr0marx@users.noreply.github.com>
2023-06-13 10:06:36 -0300

    Create forca.h

---
+++ LP/Strings/ex11/forca.h
@@ -0,0 +1,9 @@
+void abertura();
+void escolhepalavra();
+int jachutou(char letra);
+void desenhaforca();
+void chuta();
+int acertou();
+int enforcou();
+void adicionapalavra();
+void escolhepalavra();
